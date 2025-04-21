9eaaac1bdf0f9de2639ef6c7457c5c929c07fa61
Sungkar Bolat <sungkarb@Sungkars-MacBook-Pro-3.local>
2025-04-21 18:24:09 -0500

    Added another test file

--- src/utilities.py
+++ src/utilities.py
@@ -1,4 +1,9 @@
+import numpy as np
 from geopy import distance
+from pyproj import Transformer
+
+GPS_CRS="EPSG:4326"
+POINT_CRS="EPSG:32612"
 
 def dist_gps(point_1: tuple[float], point_2: tuple[float]) -> float:
     """
@@ -12,3 +17,26 @@ def dist_gps(point_1: tuple[float], point_2: tuple[float]) -> float:
         Distance in meters between the two points.
     """
     return distance.distance(point_1[:2], point_2[:2]).m
+
+def getcsv(pdal_json: str):
+    """
+    Reads json and gets the csv file from the 
+    """
+
+"""
+Converts points in the (x, y, z) format from laz format to GPS format
+
+Args:
+    points - numpy array of dimension (n x 3) with each row being a tuple of lat,
+             lon, alt
+    source_crs - source coordinate reference system for the points
+Returns:
+    List of points in the format (n x 3) where each point is now referencing GPS
+    reference system
+"""
+def to_gps(points: np.ndarray, source_crs=POINT_CRS) -> np.ndarray:
+    transformer = Transformer.from_crs(source_crs, GPS_CRS, always_xy=True)
+    result = np.array([transformer.transform(x, y, z) for x, y, z in points])
+    result[:, [0, 1]] = result[:, [1, 0]]
+    return result
+
